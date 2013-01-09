ffc91be93e100f24cb7dcc5483f53df22ab288fc
Tim Phillips <tdphilli@ualberta.ca>
2013-01-09 15:49:26 -0700

    Adding project pages for timelapses and film fest website.

---
+++ projects/filmfest/index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+    <title>Tim Phillips | ABE Film Festival Website</title>
+    <link REL=StyleSheet HREF="../../style/stylesheet.css" TYPE="text/css" MEDIA=screen>
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.2.6/jquery.min.js"></script>
+    <script type="text/javascript" src="../../javascript/javascript.js"></script>
+    <style>
+        #highlighted_link a:link {padding: 20px 30px;color:#575656;text-decoration:none;background-color:#82e3e7;}
+        #highlighted_link a:visited {padding: 20px 30px;color:#575656;text-decoration:none;background-color:#82e3e7;}
+        #highlighted_link a:hover {color:black;text-decoration:none;background-color:#40c6cb; }
+        #highlighted_link a:active {color:#575656;text-decoration:none;}
+    </style>
+</head>
+
+<body>
+
+<?php include($_SERVER['DOCUMENT_ROOT'].'/includes/header.php'); ?>
+
+<div id="content">
+
+    <img alt="ABE Film Festival" src="images/banner.jpg" class="banner">
+    <h3>ABE Film Festival Website</h3>
+
+    <table>
+        <tr>
+            <td>
+                <p>I worked on this website for a film festival at my high school. I was one of the four principal organizers for the 2007 event. The website provided information for students on news, rules, and other film resources. It was one of my first exposures to html, and while it is currently very outdated, it was a fun experience.</p>
+                <p>The site is now offline, but you can visit an older version below.</p>
+                <br><br><br>
+                <p id="highlighted_link"><a href="about.html" target="_blank">Visit the site!</a></p>
+            </td>
+            <td><img alt="ABE Film Fest Website" src="screenshot.gif" ALIGN="right"></td>
+        </tr>
+    </table>
+
+    </div>
+
+<?php include($_SERVER['DOCUMENT_ROOT'].'/includes/footer.php'); ?>
+
+</body>
+</html>
+
+
