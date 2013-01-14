c75da4ebb1098a4387ea5232865e76e7357035d3
Tim Phillips <tdphilli@ualberta.ca>
2013-01-14 16:27:29 -0700

    Adding project page for guitar videos.

---
+++ projects/guitar/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+    <title>Tim Phillips | Guitar</title>
+    <link REL=StyleSheet HREF="../../style/stylesheet.css" TYPE="text/css" MEDIA=screen>
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.2.6/jquery.min.js"></script>
+    <script type="text/javascript" src="../../javascript/javascript.js"></script>
+</head>
+<body>
+
+<?php include($_SERVER['DOCUMENT_ROOT'].'/includes/header.php'); ?>
+
+<div id="content">
+    <img alt="Guitar" src="images/banner.jpg" class="banner">
+    <h3>Guitar</h3>
+    <p>I've been playing guitar for about four years now. Here are a few videos of me playing some songs.</p>
+    <p>Most of my new guitar videos are just posted on my <a href="/thoughts/">blog</a> now.</p>
+    <p><b>Goodbye Blue Sky (Pink Floyd)</b></p>
+    <p><embed width="432" height="260" type="application/x-shockwave-flash" allowFullscreen="true" allowNetworking="all" wmode="transparent" src="http://static.photobucket.com/player.swf" flashvars="file=http%3A%2F%2Fvid24.photobucket.com%2Falbums%2Fc33%2FBesharen%2FGuitar%2FMovieon2010-02-15at0029.flv"></p>
+    <p><b>Here Comes The Sun (The Beatles)</b></p>
+    <p><embed width="432" height="260" type="application/x-shockwave-flash" allowFullscreen="true" allowNetworking="all" wmode="transparent" src="http://static.photobucket.com/player.swf" flashvars="file=http%3A%2F%2Fvid24.photobucket.com%2Falbums%2Fc33%2FBesharen%2FGuitar%2FMovieon2010-02-14at2316.flv"></p>
+    <p><b>Dead Flag Blues (GY!BE)</b></p>
+    <p>Playing with loops and Godspeed You Black Emperor!'s song <a href="http://www.youtube.com/watch?v=oOYdQOLkqf4" target="_blank">Dead Flag Blues</a> (starting at the part <a href="http://www.youtube.com/watch?v=LvrLpLRLF2c" target="_blank">10 minutes</a> into the song). I wish I had a loop pedal or some software so I could just press a button instead of having to stop each time, and then I could do it all at once. And I wish I had a violin player in my band. This was an experiment more than anything.</p>
+    <p><embed width="600" height="361" type="application/x-shockwave-flash" allowFullscreen="true" allowNetworking="all" wmode="transparent" src="http://static.photobucket.com/player.swf" flashvars="file=http%3A%2F%2Fvid24.photobucket.com%2Falbums%2Fc33%2FBesharen%2FThe_Dead_Flag_Blues.mp4"></p>
+    <p><b>Edge Of The World (Josh Ritter)</b></p>
+    <p>I did this one as another experiment in making a video of me playing a bunch instruments over top of one another. It ended up becoming repetitive pretty quickly so I'm planning to redo this and plan it out a little more first (also, I can't play piano).</p>
+    <p><embed width="600" height="361" type="application/x-shockwave-flash" allowFullscreen="true" allowNetworking="all" wmode="transparent" src="http://static.photobucket.com/player.swf" flashvars="file=http%3A%2F%2Fvid24.photobucket.com%2Falbums%2Fc33%2FBesharen%2FGuitar%2FEdgeOfTheWorld_done.flv"></p>
+</div>
+
+<?php include($_SERVER['DOCUMENT_ROOT'].'/includes/footer.php'); ?>
+
+</body>
+</html>
